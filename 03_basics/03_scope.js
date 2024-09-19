a9a06e23d024a5774d713b34a6784c43686c20b0
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-09-19 13:36:41 +0530

    add some code in scope file

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -24,3 +24,50 @@ if (true) {
 console.log(a);
 // console.log(b);
 // console.log(c); // var variable print it 30
+
+
+
+// nested scope
+
+function one(params) {
+    username = "Deepak"
+
+    function two(params) {
+        const website = "youtube" 
+        console.log(username);
+        
+    }
+    // console.log(website); //  website is under the scope of the function two
+    
+    two()
+} 
+
+one() 
+
+
+
+if (true) {
+    const myname = "DeepakYadav"
+    if (myname === "DeepakYadav") {
+        const website = " google.com"
+        console.log(myname + website);
+    }
+    // console.log(website); // it give error
+}
+// console.log(myname); // it give error
+
+
+// +++++++++++++++++++++++++++ ineresting Hoisting ++++++++++++++++++++++++++++++
+
+console.log(addone(5)); 
+function addone(num) { // it is called function
+    return num + 1
+}
+// console.log(addone(5));
+
+console.log(addtwo(5)); //it give error because of const addtwo  method
+const addtwo = function(num) { // it is also calles function expression
+    return num + 2
+}
+// console.log(addtwo(5));
+
