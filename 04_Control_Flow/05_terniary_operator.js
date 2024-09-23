5e0b7d784a8c285483ea403bc5ab1eda5d7e638c
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-09-23 08:11:52 +0530

    Terniary operator

---
+++ 04_Control_Flow/05_terniary_operator.js
@@ -0,0 +1,8 @@
+
+// Terniary Operator
+//syntax: condition ? true statement part : false part
+
+const iceTeaPrice = 150
+
+iceTeaPrice >= 100 ? console.log("less than 100") : console.log("more than 100");
+
