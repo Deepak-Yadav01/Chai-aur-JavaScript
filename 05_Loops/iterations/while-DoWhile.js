8c4adb99ce00d0fd9beca2ae5ef3a64fa0b643e9
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-09-23 18:34:05 +0530

    while and do while concept

---
+++ 05_Loops/iterations/while-DoWhile.js
@@ -0,0 +1,24 @@
+// while
+
+let index = 0
+while (index <= 30) {
+    console.log(`value of index ${index}`);
+    index = index + 2
+}
+
+
+let myArray = ["flash", "batman", "superman", "saktiman", "krish"]
+let arr = 0
+while (arr <myArray.length) {
+    console.log(`${myArray}`);
+    arr++
+}
+
+
+// do- while loop
+
+let score = 21
+do {
+    console.log(`score is ${score}`);
+    score = score + 1
+} while (score <= 20);
