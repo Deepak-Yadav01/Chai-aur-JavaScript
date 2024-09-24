7ec9b119e2b5b8f3a780749cda1b750db547f144
Deepak Yadav <dspyadavdeepak01@gmail.com>
2024-09-24 12:39:14 +0530

    add some code in the 06_for_in file

--- 05_Loops/iterations/06_for_in.js
+++ 05_Loops/iterations/06_for_in.js
@@ -16,6 +16,22 @@ for (const key in myObject) {
 const programming = ["c", "py", "js", "react", "java"]
 
 for (const key in programming) {
-    console.log(key);// it gives the index value
+    // console.log(key);// it gives the index value 
+    console.log(programming[key]);
+}
+
+
+
+// for in loop om map
+
+const map = new Map();
+ map.set('In', "India")
+ map.set('US', "United State of America")
+ map.set('FR', "france")
+ map.set('In', "India")// map me unique values hi aati h
+ map.set('RS', "Russia")
+ 
+for (const key in map) {
+    console.log(key);
     
 }
