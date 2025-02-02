18739c3e79446bd888dd1103f84824262121cb30
Deepak Yadav <dspyadavdeepak01@gmail.com>
2025-02-02 21:41:05 +0530

    How Function defines and works

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,38 +8,41 @@ function sayMyName() {
 }
 sayMyName() //reference for exicution
 
-// function addTwoNumbers (number1, number2){
-//     console.log(number1 + number2);
-// }
-// addTwoNumbers() // Nan
-// addTwoNumbers(4, 5) // 9
-// addTwoNumbers(3, "4") // 34
-// addTwoNumbers(3, "a") // 3a
-
-// addTwoNumbers(3, 2) //5
-// const result1 = addTwoNumbers(3, 2) //5
-// console.log("result:", result1);// result: undefined
-
 function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumbers() // NaN
+addTwoNumbers(4, 5) // 9
+addTwoNumbers(3, "4") // 34
+addTwoNumbers(3, "a") // 3a
+
+addTwoNumbers(3, 2) //5
+const result1 = addTwoNumbers(3, 2) //5
+
+//function ke andr jo console h wo bs output ko show krta h,return nhi krta aur return nhi kiya to jo result1 variables h wo undefined h
+console.log("result:", result1);// result: undefined
+
+
+function addTwoNumber (num1, num2){
     
-    let result = number1 + number2
+    let result = num1 + num2;
     console.log("Deepak"); 
     return result // function return ke baad ka code unreachable hota h aur ye ye code execute nhi hota
     console.log("yadav"); // jaise ki code ki ye line
     
-    // return number1 + number2
+    // return num1 + num2
 }
-const result = addTwoNumbers(3, 2)
+const result = addTwoNumber(3, 2)
 console.log("result:", result);// result: 5
 
 
-// function loginUserMessage(userName) {
-//     return `${userName} just logged in`
-// }
-// loginUserMessage("welcome Deepak Yadav")// here function is exicuted but there is no any code for render or displaying it
-// console.log(loginUserMessage("welcome Deepak Yadav"));
-// console.log(loginUserMessage(""));
-// console.log(loginUserMessage()); //undefined 
+function loginUserMessage(userName) {
+    return `${userName} just logged in`
+}
+loginUserMessage("welcome Deepak Yadav")// here function is exicuted but there is no any code for render or displaying it
+console.log(loginUserMessage("welcome Deepak Yadav"));
+console.log(loginUserMessage(""));
+console.log(loginUserMessage()); //undefined 
 
 
 
